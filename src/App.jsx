b9b41be9a9c78b9c955ade3124756f56a3756ce0
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-05-23 02:27:00 -0300

    Move ItemListContainer component to a new ItemListContainer directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar.jsx'
-import ItemListContainer from './components/ItemListContainer.jsx'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
 
 function App() {
   return (
