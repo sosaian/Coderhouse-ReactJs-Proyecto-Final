f50b6093ed8facd9a712b222743a718e7cd5ac33
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-05 02:16:01 -0300

    Import react-router-dom into App.jsx with <Route> element inside <main>

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar.jsx'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header>
         <NavBar/>
       </header>
       <main>
-        <ItemListContainer greeting={"¡UPS! Parece que no hay productos disponibles ahora..."}/>
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting={"¡UPS! Parece que no hay productos disponibles ahora..."}/>} />
+        </Routes>
       </main>
       <footer>
         REACT JS | Coderhouse | Pre-entrega 2 | Ian Sosa
       </footer>
-    </>
+    </BrowserRouter>
   )
 }
 
