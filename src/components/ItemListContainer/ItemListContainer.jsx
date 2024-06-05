01a270d434822e60a234e49a693f3f59963fac9d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-05 05:50:18 -0300

    Add async-mock implementation to bring the product list
    Add loading state management

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,23 @@
 import './ItemListContainer.css'
+import { getProductList } from '../../data/async-mock.js'
+import { useEffect, useState } from 'react'
+import { ItemList } from '../ItemList/ItemList.jsx'
 
-function ItemListContainer({ greeting })
+function ItemListContainer()
 {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getProductList()
+            .then(response => setProducts(response))
+            .catch(err => console.error(err))
+            .finally(() => setLoading(false))
+    }, [])
+
     return (
         <>
-            <h2>{greeting}</h2>
+            {loading ? <h2>Cargando productos...</h2> : (products ? <ItemList products={products} /> : <h2>¡UPS! Parece que no hay productos disponibles ahora...</h2>)}
         </>
     )
 }
