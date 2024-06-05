3c963b9339bdf71ca6b2aaed2298c092a65988bf
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-05 05:39:28 -0300

    Create and Add Item component
    These will be the product cards

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,19 @@
+import './Item.css'
+import { Link } from 'react-router-dom'
+
+export function Item({ id, category, img, name, price })
+{
+    return (
+        <>
+            <li>
+                <div>{category}</div>
+                <img src={img.default} />
+                <div>{name}</div>
+                <div>{price}</div>
+                <Link to={`/item/${id}`}>
+                    <button>Ver Detalle</button>
+                </Link>
+            </li>
+        </>
+    )
+}
