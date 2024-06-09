2a66309d4c4ef805e8e538e2c3f290999cf7e769
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-09 06:25:40 -0300

    Add item/:itemId route enabling itemDetailContainer functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { NavBar } from './components/NavBar/NavBar.jsx'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer.jsx'
+import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
           <Route path="/Contacto" element={<h2>{WIP_MESSAGE}</h2>} />
           <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
         </Routes>
