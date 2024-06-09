873ecfd4b200c21c60bd060319bdbe8a7dccc370
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-09 16:30:58 -0300

    Remove category in Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,11 @@
 import './Item.css'
 import { Link } from 'react-router-dom'
 
-export function Item({ id, category, img, name, price })
+export function Item({ id, img, name, price })
 {
     return (
         <>
             <li>
-                <div>{category}</div>
                 <img src={img} />
                 <div>{name}</div>
                 <div>{`$ ${price}`}</div>
