96592dfd217c775f426afe11c93aa88607df8a56
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-09 16:44:30 -0300

    Add basic CSS styling for this low-fidelity verison of ItemCount
    Change + and - for emojis... ideally this would be and icon or svg

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,10 +3,10 @@ import './ItemCount.css'
 export function ItemCount({ quantity, handleClick })
 {
     return(
-        <div>
-            <button onClick={() => handleClick("minus")}>-</button>
+        <div id="itemCount">
+            <button onClick={() => handleClick("minus")}>➖</button>
             <div>{quantity}</div>
-            <button onClick={() => handleClick("plus")}>+</button>
+            <button onClick={() => handleClick("plus")}>➕</button>
         </div>
     )
 }
