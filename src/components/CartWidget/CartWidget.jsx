f31ade1d8842265dc60dca497e5f673b98467494
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-20 03:08:36 -0300

    Change the cart badge content to reflect the total quantity of items in the cart instead of the number of different products

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,14 +1,22 @@
 import './CartWidget.css'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../../context/CartContext'
 
 export function CartWidget()
 {
     const { cart, showCartProducts } = useContext(CartContext)
+    const [ quantity, setQuantity ] = useState()
+
+    useEffect(() => {
+        const cartQuantity = cart.reduce((accumulator, product) => accumulator + product.quantity, 0);
+        
+        setQuantity(cartQuantity === 0 ? undefined : cartQuantity)
+    }, [cart])
+
 
     return (
         <>
-            <button onClick={showCartProducts}>🛒<span>{cart.length}</span></button>
+            <button onClick={showCartProducts}>🛒<span>{quantity}</span></button>
         </>
     )
 }
