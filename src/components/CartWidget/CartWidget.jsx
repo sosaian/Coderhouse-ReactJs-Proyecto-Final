ffad13b6a00cb3672c61cabb0855a379452847ae
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-23 21:40:59 -0300

    Add Link to CartWidget for /cart path

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,22 +1,24 @@
 import './CartWidget.css'
 import { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom'
 
 export function CartWidget()
 {
-    const { cart, showCartProducts } = useContext(CartContext)
+    const { cart } = useContext(CartContext)
     const [ quantity, setQuantity ] = useState()
 
     useEffect(() => {
         const cartQuantity = cart.reduce((accumulator, product) => accumulator + product.quantity, 0);
-        
         setQuantity(cartQuantity === 0 ? undefined : cartQuantity)
     }, [cart])
 
 
     return (
         <>
-            <button onClick={showCartProducts}>🛒<span>{quantity}</span></button>
+            <Link to={'/cart'}>
+                <button>🛒<span>{quantity}</span></button>
+            </Link>
         </>
     )
 }
