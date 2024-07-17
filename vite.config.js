9275ec9b8175821cc30582a058cec9f24891b489
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-07-16 21:08:06 -0300

    Install and Add dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+
+config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ProyectoFinalSOSA/',
   plugins: [react()],
+
+  define: {
+    'process.env': process.env
+  }
 })
